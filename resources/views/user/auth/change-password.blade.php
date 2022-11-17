2bccf32a1f2df1014b0e276ecb332d0a62951f11
ielemson <ielemson@gmail.com>
2022-11-17 05:49:05 +0100

    updated reset password view

--- resources/views/user/auth/change-password.blade.php
+++ resources/views/user/auth/change-password.blade.php
@@ -1,27 +1,21 @@
-@extends('frontend.layouts.frontend')
+@extends('frontend.layouts.beautykinkLayout')
 @section('title')
     {{ __('Password Reset') }}
 @endsection
 @section('content')
     <!-- Page Title-->
-    <div class="page-title">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    {{-- <ul class="breadcrumbs">
-                        <li><a href="{{ route('frontend.index') }}">{{ __('Home') }}</a> </li>
-                        <li class="separator"></li>
-                        <li>{{ __('Password Reset') }}</li>
-                    </ul> --}}
-                </div>
-            </div>
-        </div>
-    </div>
+    @include('frontend._inc.header_single_page', ['title_1' => 'Account', 'title_2' => 'Reset Password'])
     <!-- Page Content-->
-    <div class="container padding-bottom-3x mb-1">
-        <div class="row justify-content-center">
-            <div class="col-lg-8 col-md-10">
-                <form class="card mt-4" method="POST" action="{{ route('user.change.password') }}">
+   
+    <section>
+        <div class="container" data-padding-top="62"> 
+          <h4 class="fz-24 mb-25">Reset Password</h4>
+          <div class="row">
+            <div class="col-12">
+              <div class="register-form-content">
+                <div class="register-form">
+                  <span class="login-account">Already have an account? <a href="{{route('user.login')}}"><b>Log in instead!</b></a></span>
+                  <form class="card mt-4" method="POST" action="{{ route('user.change.password') }}">
                     @csrf
                     <input type="hidden" name="file_token" value="{{ $token }}">
                     <div class="card-body">
@@ -43,7 +37,16 @@
                         <button class="btn btn-primary btn-sm" type="submit">{{ __('Reset Password') }}</button>
                     </div>
                 </form>
+                </div>
+                </div>
             </div>
+          </div>
         </div>
-    </div>
+      </section>
+@include('frontend._inc.divider')
+
 @endsection
+
+{{-- @section('styleplugins')
+    <link id="mainStyles" rel="stylesheet" media="screen" href="{{ asset('frontend/css/styles.min.css') }}">
+@endsection --}}
