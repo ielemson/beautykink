5213cb6cbdda4b32f1caf8b6da30ffb8b8fe46d4
ielemson <ielemson@gmail.com>
2022-12-30 12:28:23 +0100

    embedded image to rstock message

--- resources/views/emails/CustomerRestockEmail.blade.php
+++ resources/views/emails/CustomerRestockEmail.blade.php
@@ -3,7 +3,7 @@
 
 {!! $MsgToCustomer !!}
 <a style="align-items: center" href="{{ $link }}">
-<img src="{{asset($img)}}" style="width:100%; text-align:center !important;" alt="Product"></a>
+<img src="{{asset($img)}}" style="width:50%; text-align:center !important;" alt="Product"></a>
 <br/><br/>Thanks,<br/>
 {{ config('app.name') }}
 
