0ac9a213fcdf755c8474a3bd53e81d3b466171e9
ielemson <ielemson@gmail.com>
2023-01-22 23:10:51 +0100

    Category update

--- app/Http/Controllers/Frontend/CatalogController.php
+++ app/Http/Controllers/Frontend/CatalogController.php
@@ -185,7 +185,7 @@ class CatalogController extends Controller
             $query->where('status', 1);
         }])->get();
 
-        $items = Item::where('897',$category->id)->paginate(10);
+        $items = Item::where('category_id',$category->id)->paginate(10);
        
         return view('frontend.catalog.index',compact('items','categories','category'));
     
